b7a759b4bf1500c9c113b5149c5b6e9151a07439
Marina Zinchenko <iolsoofuolso@gmail.com>
2024-01-27 20:34:06 -0800

    add log ut btn and log out handler and add savind submitted formData in ref element

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import login from '../utilities/login.js';
 
 export default function Login() {
   const [formData, setFormData] = useState({ username: '', password: '' });
   const [isLoading, setIsLoading] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [error, setError] = useState(null);
+
+  const submittedFormData = useRef();
+
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
     login(formData)
       .then((res) => {
+        submittedFormData.current = formData;
         setFormData((prevVal) => ({...prevVal, username: '', password: '' }));
         setError(null);
-        console.log(res);
+        setIsLoggedIn(true);
+        console.log(res); // Success! Meow!
       })
       .catch((e) => {
         setError(e)
@@ -24,13 +30,22 @@ export default function Login() {
 
   const handleInputChange = (e) => {
     const fieldName = e.target.name;
-    console.log(e.target.name);
     setFormData((prevVal) => ({...prevVal, [fieldName]: e.target.value}))
+  };
+
+  const handleLogOut = () => {
+    setIsLoggedIn(false);
   };
 
   return (
     <div className="App">
       <div className="login-container">
+        {isLoggedIn ? (
+          <>
+            <h1>Privet, {submittedFormData.current.username}!</h1>
+            <button type="button" onClick={handleLogOut}>Log out</button>
+          </>
+        ) : (
         <form className="form" onSubmit={handleFormSubmit}>
           {error ? <p className="error">Please eneter right format data</p> : ''}
           <p>It is time to Login...</p>
@@ -54,7 +69,7 @@ export default function Login() {
           <button className="btn-submit" type="submit" disabled={isLoading}>
             {isLoading ? 'Logging in...' : 'Log in'}
           </button>
-        </form>
+        </form>)}
       </div>
     </div>
   );
